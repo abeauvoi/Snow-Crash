0ab73cc2bc04c41cc99cc97c0f0d936032c66eb1
Alexandre BEAUVOIS <abeauvoi@student.42.fr>
2020-03-08 15:50:55 +0100

    add explanation

--- level09/Ressources/incremental_rot_reverse.py
+++ level09/Ressources/incremental_rot_reverse.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+# A test run of the binary gives :
+# level09@SnowCrash:~$ ./level09 token
+# tpmhr
+# We can see that the first letter is unchanged, by each following letter is increasingly
+# further away from the input string.
+# p is 1 after o, m is 2 after k, h is 3 after e, etc.
+# We figured out that we needed to pass the content of the file token to the following script
 import sys
 
 def	rot_reverse(input):

